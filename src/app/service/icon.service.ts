8f0f64f212e25594de360165bcbcef806b6b9f39
Hisham PP <mohammedhisham@kiebot.com>
2024-09-08 23:13:43 +0530

    new addition added stats

---
+++ src/app/service/icon.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import iconsArray from '../constants/icons';
+
+interface Icons {
+  id: number;
+  icon: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IconService {
+  private iconArraySubject = new BehaviorSubject<Icons[]>([]);
+  private selectedItemsSubject = new BehaviorSubject<Icons[]>([]);
+  private removedItemsCount = new BehaviorSubject<number>(0);
+
+  iconArray$ = this.iconArraySubject.asObservable();
+  selectedItems$ = this.selectedItemsSubject.asObservable();
+  removedItemsCount$ = this.removedItemsCount.asObservable();
+
+  constructor() {
+    // Initialize iconArray with default icons
+    this.iconArraySubject.next(iconsArray);
+  }
+
+  handleOnIconClick(icon: Icons) {
+    const currentItems = this.selectedItemsSubject.value;
+    let updatedItems: Icons[];
+
+    if (currentItems.includes(icon)) {
+      // If the item is already selected, remove it
+      updatedItems = currentItems.filter((item) => item !== icon);
+    } else {
+      // If the item is not selected, add it
+      updatedItems = [...currentItems, icon];
+      if (updatedItems.length > 3) {
+        // Remove the first item if there are more than 3
+        updatedItems.shift();
+      }
+    }
+
+    // Update selected items
+    this.selectedItemsSubject.next(updatedItems);
+
+    // Check if all selected items are the same
+    if (
+      updatedItems.length === 3 &&
+      updatedItems.every((item) => item.icon === updatedItems[0].icon)
+    ) {
+      // Remove the item from iconArray
+      const itemToRemove = updatedItems[0].icon;
+      const updatedIconArray = this.iconArraySubject.value.filter(
+        (item) => item.icon !== itemToRemove
+      );
+
+      // Update the iconArray and increment the removedItemsCount
+      this.iconArraySubject.next(updatedIconArray);
+
+      this.selectedItemsSubject.next([]);
+      this.removedItemsCount.next(this.removedItemsCount.value + 3);
+    }
+  }
+}
